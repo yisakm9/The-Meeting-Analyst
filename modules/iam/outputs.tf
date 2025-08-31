1c6fb717b0f77ec2e7617f1d3f9c2f01f97fc27f
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-08-31 11:29:03 +0300

    Remove redundant output for Lambda execution role ARN from IAM module

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -6,11 +6,6 @@ output "lambda_execution_role_arn" {
 }
 # modules/iam/outputs.tf
 
-output "lambda_execution_role_arn" {
-  description = "The ARN of the IAM role for the Lambda function."
-  value       = aws_iam_role.lambda_execution_role.arn
-}
-
 output "transcribe_service_role_arn" {
   description = "The ARN of the IAM role for the Transcribe service to assume."
   value       = aws_iam_role.transcribe_service_role.arn
